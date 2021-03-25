91b55bc3dc326248e4b58ecbba51e01fe561dbd0
HanSangKkyu <kkyu67@naver.com>
2021-03-25 17:14:00 +0900

    sd

--- 07simple_spin/simple_spin.h
+++ 07simple_spin/simple_spin.h
@@ -3,7 +3,7 @@
 #define IOCTL_NUM2 IOCTL_START_NUM + 2
 #define SIMPLE_IOCTL_NUM 'z'
 #define SIMPLE_SPIN_READ _IOWR (SIMPLE_IOCTL_NUM, IOCTL_NUM1, unsigned long *)
-#define SIMPLE_SPIN_WRITE _IOWR (SIMPLE_IOCTL_NUM, IOCTL_NUM2, unstgned long *)
+#define SIMPLE_SPIN_WRITE _IOWR (SIMPLE_IOCTL_NUM, IOCTL_NUM2, unsigned long *)
 struct str_st {
 	int len; 
 	char str [128];
